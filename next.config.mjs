86951e043339662de69bff3c656184f1c4214c0f
Ritik Upadhyay <ritikupadhyay838@gmail.com>
2026-06-21 18:33:21 +0530

    chore: limit nextjs build and runtime cpus to 1 worker to prevent OOM crash loop

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    cpus: 1,
+    workerThreads: false
+  }
 };
 
 export default nextConfig;
